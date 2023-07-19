feb7efa50f64ab638fd08829fefef7eae322403c
osamame <tokjohn@gmail.com>
2023-07-19 23:20:41 +0100

    First Commit 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { AccountComponent } from './account/account.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [ 
-  {path : "", component: LoginComponent},
-  {path:"login", component: LoginComponent},
+  //{path : "", component: LoginComponent},
+  //{path:"login", component: LoginComponent},
+  {path : "", component: AccountComponent},
+  {path:"dashboard", component: AccountComponent},
   {
     path: 'account',
     loadChildren: () => import('./account/account.module').then(m => m.AccountModule),
